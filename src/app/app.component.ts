d74454bfbe083621532b4a3f73b721dedca8dd8b
Leonardo Alves <leonardo.alves@cactoweb.com.br>
2024-02-21 00:46:35 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet],
+  template: `Meu<router-outlet></router-outlet>Caramelo`
 })
 export class AppComponent {
-  title = 'meu-caramelo';
 }
