52fbaee943b32c5c8307549686dce9939897e5fe
Leonardo Alves <leonardo.alves@cactoweb.com.br>
2024-02-25 01:01:53 -0300

    update header component

--- src/app/modules/website/components/header/header.component.ts
+++ src/app/modules/website/components/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+// Angular Material
+import {MatIconModule} from '@angular/material/icon';
+
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
