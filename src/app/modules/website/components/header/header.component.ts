99f9d1f937c479e7aaa7e07f1da7434086b04826
Leonardo Alves <leonardo.alves@cactoweb.com.br>
2024-02-26 00:42:23 -0300

    update header component / home page

--- src/app/modules/website/components/header/header.component.ts
+++ src/app/modules/website/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, signal } from '@angular/core';
 
 // Angular Material
 import {MatIconModule} from '@angular/material/icon';
@@ -6,10 +7,37 @@ import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  menuListOrder = ["INÍCIO", "CADASTRAR", "ENCONTRE UM AMIGO", "SOBRE NÓS", "CONTATOS"];
+  
+  public menuStatus = signal(false);
+  public toggleIcon: string = 'menu';
+  public innerWidth: any;
+
+  ngOnInit() {
+    this.innerWidth = window.innerWidth; 
+  }
+
+  @HostListener('window:resize', ['$event']) 
+  onResize(event: any): void {
+    this.innerWidth = window.innerWidth;
+  }
+
+  menuOpen() {
+    if (this.menuStatus()) {
+      this.menuStatus.set(false);
+      this.toggleIcon = 'menu';
+      
+    } else {
+      this.menuStatus.set(true);
+      this.toggleIcon = 'close';
+    }
+
+
+  }
 }
